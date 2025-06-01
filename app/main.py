14286fd03ab5ff3873e2f9f2954e0a133ffe515e
AIInspire <omniaabdulnabii@gmail.com>
2025-06-02 00:17:01 +0300

    Commit

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
 from prometheus_client import Counter, Histogram, generate_latest
 from app.schemas import GestureInput, PredictionOutput
 from app.predict import predict_gesture
 import time
 
 app = FastAPI(title="Hand Gesture Classifier", version="1.0")
+
+# -----------------------
+# CORS Middleware Setup
+# -----------------------
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Use ["http://localhost:8888"] in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # -----------------------
 # Prometheus Metrics Setup
@@ -31,7 +43,6 @@ async def track_requests(request: Request, call_next):
 # -----------------------
 # Routes
 # -----------------------
-
 @app.get("/")
 def read_root():
     return {"message": "Hand Gesture Classification API is up and running!"}
